d43c56db129cbe6e62f24191904e38078342bd67
Liam Labuschagne <=>
2024-06-21 11:49:49 +1200

    Use expressions to render variables in JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  const name = "Liam";
+  const x = 10;
+  const y = 20;
+  return (
+    <>
+      <div className="text-5xl">App</div>
+      <p>Hello {name}</p>
+      <p>
+        The sum of {x} and {y} is {x + y}
+      </p>
+    </>
+  );
 };
 export default App;
