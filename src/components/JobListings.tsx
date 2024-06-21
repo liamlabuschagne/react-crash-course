4cfa415bba0f36f4bbf81660870f086b51f3d540
Liam Labuschagne <=>
2024-06-21 18:52:01 +1200

    Rename recentJobs to jobListings

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -2,7 +2,7 @@ import jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
 const JobListings = ({ isHome = false }: { isHome?: boolean }) => {
-  const recentJobs = isHome ? jobs.slice(0, 3) : jobs;
+  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
   return (
     <>
       <section className="bg-blue-50 px-4 py-10">
@@ -11,7 +11,7 @@ const JobListings = ({ isHome = false }: { isHome?: boolean }) => {
             Browse Jobs
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {recentJobs.map((job) => {
+            {jobListings.map((job) => {
               return <JobListing key={job.id} job={job} />;
             })}
           </div>
