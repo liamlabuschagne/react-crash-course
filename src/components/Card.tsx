e7e049068f5b218b468236b2e38c74a6ba2fd5b4
Liam Labuschagne <=>
2024-06-21 16:39:52 +1200

    Make background class a prop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,11 @@
-const Card = ({ children }: { children: any }) => {
-  return <div className="bg-grey-100 p-6 rounded-lg shadow-md">{children}</div>;
+const Card = ({
+  children,
+  bg = "bg-gray-100",
+}: {
+  children: any;
+  bg?: string;
+}) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
 };
 
 export default Card;
