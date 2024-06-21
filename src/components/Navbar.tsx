93a82c08cc312e6727f2b8247c0104fa833f1715
Liam Labuschagne <=>
2024-06-21 18:26:38 +1200

    Add jobs page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
                     Home
                   </a>
                   <a
-                    href="/jobs.html"
+                    href="/jobs"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Jobs
