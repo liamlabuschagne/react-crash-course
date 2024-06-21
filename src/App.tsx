ff1e4a3ba39ad05391837053b66cc986c6f5bd83
Liam Labuschagne <=>
2024-06-21 12:03:52 +1200

    Create first empty component and import it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <>
+      <Navbar />
       <nav className="bg-indigo-700 border-b border-indigo-500">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
