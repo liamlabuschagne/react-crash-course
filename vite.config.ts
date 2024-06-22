5c393887c0bc8c21475f276796904967c1ba6d81
Liam Labuschagne <=>
2024-06-22 14:54:55 +1200

    Remove server block from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/react-crash-course/",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        rewrite: (path: string) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
